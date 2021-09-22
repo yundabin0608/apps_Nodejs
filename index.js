23737f8a73dc919cf0ec34277e5d78bd55f6b1ef
yoondabin <yundabin0608@gmail.com>
2021-09-23 02:52:44 +0900

    로그인 기능 구현 - Bcrypt & sjonwebtoken

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const app = express()
 const port = 5000  
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const {User} =require("./models/User");
 const config = require('/config/key');
 // application /x-www-from-urlencoded를 분석해서 가지고 올 수 있게 함  밑에는 json 형식
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 mongoose.connect(config.mongoURI)
   .then(()=> console.log('MongoDB connected!'))
   .catch(err => console.log(err))
@@ -30,6 +31,38 @@ app.post('/register', (req, res)=>{
             success:true
         })
     })
+})
+
+app.post('/login', (req, res)=>{
+
+    //요청된 이메일이 DB에 있는지 찾기
+    User.findOne({email:req.body.email}, (err,user)=> {
+        if(!user){
+            return res.json({
+                loginSuccess:false,
+                 message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+    //있다면 비번이 맞는 비번인지 확인 
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) //=비번 틀렸다는것
+                return res.json({loginSuccess:false, message:"비밀번호가 틀렸습니다."})
+            //맞다면 토큰 생성
+            user.generateToken((err,user)=>{
+                if (err) return res.status(400).send(err);
+                
+                // 토큰을 쿠키나 로컬스토리지등에 저장해둘것
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSuccess:true, userId: user._id})
+
+            })
+        })
+    })
+   
+
+    
+
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcrypt');
 const saltRounds = 10
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -47,6 +48,29 @@ userSchema.pre('save', function(next){
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // plainPassword 가 1234~ DB의 암호화된 비번 !@42ㄱ34@#$ 이런거랑 맞는지 체크할것
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cd){
+    //jsonwebtoken을 이용해서 토큰 생성
+
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // jwt.sign 시 첫번째 인자가 object이어햐 하므로 toHexString()을 추가적으로 해줌
+    
+    // user._id + 'seceretToken' 형태로 토큰 만듦
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
 
 const user = mongoose.model('User', userSchema)
 module.exports={User}
