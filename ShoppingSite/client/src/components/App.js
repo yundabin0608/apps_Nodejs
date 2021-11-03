3f3a37a9afe2cb13441c85100c19ec628245a7e1
yoondabin <yundabin0608@gmail.com>
2021-11-03 21:12:38 +0900

    CREATE UploadPage

--- ShoppingSite/client/src/components/App.js
+++ ShoppingSite/client/src/components/App.js
@@ -7,6 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
+import UploadProductPage from './views/UploadProductPage/UploadProductPage';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -21,6 +22,8 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} /> 
+          {/* 로그인한사람만 접근해야 하므로 true */}
         </Switch>
       </div>
       <Footer />
